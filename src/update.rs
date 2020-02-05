7b03a6c28c31a1debacbc296794574b2d37a6b38
Tom McKernan <tmeaglei@gmail.com>
2020-02-05 15:26:03 -0800

    final maybe

--- src/update.rs
+++ src/update.rs
@@ -1,41 +1,53 @@
-use crate::settings::get_settings_json;
-use std::time::SystemTime;
-use chrono::{Utc, Date, DateTime, Duration};
-use std::str::Chars;
+use crate::settings::{get_settings_json, write_settings_json};
+
+use chrono::{Utc, DateTime, Duration};
+
 use std::process::Command;
 use json::JsonValue;
 
-pub fn upgrade(name: String, force: bool) {
-    let settings_json = get_settings_json();
-    if name == "" {
-        let entries = settings_json.entries();
-        entries.for_each(|(name, value)| {
-            println!("Now running {}", name);
-            let time = DateTime::parse_from_rfc3339(value["lastUpdated"].as_str().unwrap()).unwrap();
-            let freq_str = value["frequency"].clone().to_string();
-            let frequency = freq_str.chars();
-            let time_char = frequency.clone().last().unwrap();
-            let digit = frequency.clone()
-                .take_while(|c| c.is_digit(10))
-                .collect::<String>()
-                .parse::<i64>().unwrap();
-            let duration = match time_char {
-                'd' => Duration::days(digit),
-                'w' => Duration::weeks(digit),
-                'm' => Duration::weeks(digit * 4),
-                'y' => Duration::weeks(digit * 52),
-                _ => panic!("Incorrect Format")
-            };
 
-            if time + duration < DateTime::from(Utc::now()) {
-                run_commands(value)
-            }
-        })
+pub fn upgrade(name: String, force: bool) {
+    let mut settings_json = get_settings_json();
+    if name == "" {
+        let entries = settings_json.entries_mut();
+        entries.for_each(|(name, value)| {
+            run_command(value, force, name);
+        });
+    } else {
+        run_command(&mut settings_json[name.clone()], force, name.as_str());
     }
+    write_settings_json(json::stringify(settings_json));
 }
 
 
-fn run_commands(value: &JsonValue) {
+fn run_command(value: &mut JsonValue, force: bool, name: &str) {
+    let time = DateTime::parse_from_rfc3339(value["lastUpdated"].as_str().unwrap()).unwrap();
+    let today_timestamp = JsonValue::from(Utc::now().to_rfc3339());
+    let freq_str = value["frequency"].clone().to_string();
+    let frequency = freq_str.chars();
+    let time_char = frequency.clone().last().unwrap();
+    let digit = frequency.clone()
+        .take_while(|c| c.is_digit(10))
+        .collect::<String>()
+        .parse::<i64>().unwrap();
+    let duration = match time_char {
+        'd' => Duration::days(digit),
+        'w' => Duration::weeks(digit),
+        'm' => Duration::weeks(digit * 4),
+        'y' => Duration::weeks(digit * 52),
+        _ => panic!("Incorrect Format")
+    };
+    if force {
+        run_commands(value, name);
+        value["lastUpdated"] = today_timestamp.clone();
+    } else if time + duration < DateTime::from(Utc::now()) {
+        run_commands(value, name);
+        value["lastUpdated"] = today_timestamp.clone();
+    }
+}
+
+fn run_commands(value: &JsonValue, name: &str) {
+    println!("Now running {}", name);
     value["commands"].members().for_each(|item| {
         let cmd_vec: Vec<&str> = item.as_str().unwrap()
             .split_whitespace().collect();
